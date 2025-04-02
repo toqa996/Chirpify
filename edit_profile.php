7b083285ea998d455adeb817607c6b25b129f1d8
toqa <9025432@student.zadkine.nl>
2025-04-02 13:33:43 +0200

    Update edit_profile.php
    .

--- edit_profile.php
+++ edit_profile.php
@@ -2,7 +2,7 @@
 session_start();
 include 'config.php';
 
-// Check if user is logged in
+// Check if user is logged in.
 if (!isset($_SESSION["user_id"])) {
     header("Location: login.php");
     exit;
